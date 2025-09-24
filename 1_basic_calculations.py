7246dcdd7f431c442c1371422a491367d0b0dced
lukegjpotter <lukegjpotter@gmail.com>
2025-09-24 21:18:57 +0100

    Exercise 1: Basic Calculations

---
+++ 1_basic_calculations.py
@@ -0,0 +1,17 @@
+# Triangle Area
+base = 3
+height = 4
+triangleArea = 0.5 * base * height
+print(f"Area of Triangle is: {triangleArea}")
+
+# Celsius to Fahrenheit
+celsius = 38
+fahrenheit = 9/5 * celsius + 32
+print(f"Fahrenheit is: {fahrenheit}")
+
+# Loan Interest
+principle = 100000
+interestRate = 0.6
+periodInYears = 5
+simpleInterest = (principle * interestRate * periodInYears) / 100
+print(f"Simple Interest is: €{simpleInterest}")
